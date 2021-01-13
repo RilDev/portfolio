90897999781021fe47248c9d354057393a0a9eab
RilDev <c.public@tutanota.com>
2021-01-13 17:08:17 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
 const cacheName = 'v0.0.1';
 const filesToCache = [
-    '/',
-    '/index.html',
-    '/images/github.svg',
-    '/images/linkedin.svg',
-    '/images/icons/apple-touch-icon.png',
-    '/images/icons/favicon-32x32.png',
-    '/images/icons/favicon-16x16.png',
-    '/images/open-in-new-blue.svg',
-    '/images/open-in-new-white.svg',
-    '/images/selfy.svg',
-    '/images/youtube.svg',
-    '/scripts/cookies.js',
-    '/styles/main.css',
+    './',
+    './index.html',
+    './images/github.svg',
+    './images/linkedin.svg',
+    './images/icons/apple-touch-icon.png',
+    './images/icons/favicon-32x32.png',
+    './images/icons/favicon-16x16.png',
+    './images/open-in-new-blue.svg',
+    './images/open-in-new-white.svg',
+    './images/selfy.svg',
+    './images/youtube.svg',
+    './scripts/cookies.js',
+    './styles/main.css',
 ];
 
 // install service worker and cache all the apps content
