196caa895a629e810031451767946cb4a29148bf
RilDev <c.public@tutanota.com>
2021-02-02 09:19:05 +0000

    Deploying to gh-pages from  @ a0418556c1644c2f7daa5ebfda14ed1c67982356 🚀

--- scripts/cwebp.sh
+++
@@ -1,4 +0,0 @@
-# cwebp file in images/portfolio
-for file in images/portfolio/*; do
-    cwebp -q 50 "$file" -o "${file%.*}.webp"
-done
--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
 const cacheName = 'v0.0.1';
 const filesToCache = [
-    './',
-    './index.html',
-    './images/icons/apple-touch-icon.png',
-    './images/icons/favicon-32x32.png',
-    './images/icons/favicon-16x16.png',
-    './images/portfolio/gh-packages.png',
-    './images/portfolio/code-for-faith.png',
-    './images/portfolio/rildev-s-website.png',
-    './images/github.svg',
-    './images/linkedin.svg',
-    './images/open-in-new-blue.svg',
-    './images/open-in-new-white.svg',
-    './images/portrait.svg',
-    './images/portrait1.svg',
-    './images/youtube.svg',
-    './scripts/cookies.js',
-    './styles/main.css',
+    '/',
+    'index.html',
+    'images/icons/apple-touch-icon.png',
+    'images/icons/favicon-32x32.png',
+    'images/icons/favicon-16x16.png',
+    'images/portfolio/gh-packages.png',
+    'images/portfolio/code-for-faith.png',
+    'images/portfolio/rildev-s-website.png',
+    'images/github.svg',
+    'images/linkedin.svg',
+    'images/open-in-new-blue.svg',
+    'images/open-in-new-white.svg',
+    'images/portrait.svg',
+    'images/portrait1.svg',
+    'images/youtube.svg',
+    'scripts/cookies.js',
+    'styles/main.css',
 ];
 
 // install service worker and cache all the apps content
@@ -26,7 +26,22 @@ self.addEventListener('install', event => {
         return cache.addAll(filesToCache);
     }
 
+    // imediately activate the service worker when updated
+    self.skipWaiting();
+
     event.waitUntil(preCache());
+});
+
+// delete outdated cache on cacheName update
+self.addEventListener('activate', event => {
+    const deleteOutdatedCaches = async() => {
+        const keys = await caches.keys();
+        return Promise.all(keys
+            .filter(key => key !== cacheName)
+            .map(key => caches.delete(key)));
+    }
+
+    event.waitUntil(deleteOutdatedCaches());
 });
 
 // serve cached content when offline
@@ -37,4 +52,4 @@ self.addEventListener('fetch', event => {
     };
 
     event.respondWith(cachedResponse(event));
-})
+});
